228b8f8143d9dcc844781a33b6dc8cff14be4f39
Francisco Pérez <drfperez@gmail.com>
2022-01-18 10:32:05 +0100

    Update mathjaxsteps.js
    External js

--- mathjaxsteps.js
+++ mathjaxsteps.js
@@ -1,4 +1,4 @@
- MathJax = {
+MathJax = {
     tex: {inlineMath: [['$', '$'], ['\\(', '\\)']]},
     chtml: {
       displayAlign: 'left'
@@ -16,3 +16,40 @@
     }
   };
  
+  <script type="text/javascript">
+  //
+  //  Use a closure to hide the local variable
+  //
+
+
+  (function () {
+    var n = 1;
+
+    //
+    //  Make the current step be visible, and increment the step.
+    //  If it is the last step, disable the step button.
+    //  Once a step is taken, the reset button is made available.
+    //
+    window.ShowStep = function () {
+      document.getElementById("Step" + n++).style.visibility = "visible";
+      if (!document.getElementById("Step" + n)) {
+        document.getElementById("step").disabled = true;
+      }
+      document.getElementById("reset").disabled = false;
+    }
+
+    //
+    //  Enable the step button and disable the reset button.
+    //  Hide the steps.
+    //
+    window.ResetSteps = function () {
+      document.getElementById("step").disabled = false;
+      document.getElementById("reset").disabled = true;
+      var i = 1, step; n = 1;
+      while (step = document.getElementById("Step" + i)) {
+        step.style.visibility = "hidden";
+        i++
+      }
+    }
+  })();
+  </script>
